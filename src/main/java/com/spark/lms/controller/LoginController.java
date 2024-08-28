c8b30d4f103a5734475e0429facd7496e9f78c48
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-08-28 11:14:23 +0700

    linda diuk

--- src/main/java/com/spark/lms/controller/LoginController.java
+++ src/main/java/com/spark/lms/controller/LoginController.java
@@ -1,16 +1,35 @@
 package com.spark.lms.controller;
 
+import com.spark.lms.model.Category;
+import com.spark.lms.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String loginPage() {
-		return "/login";
+	@Autowired
+	private CategoryService categoryService;
+
+	@GetMapping("/login")
+	public String loginPage(Model model) {
+		// Lấy danh sách danh mục
+		List<Category> categories = categoryService.getAll();
+
+		// Lấy sách theo từng danh mục
+		for (Category category : categories) {
+			category.setBooks(categoryService.getByCategory(category)); // Assuming you have a method in service to get books by category
+		}
+
+		model.addAttribute("categories", categories);
+
+		// Trả về view login
+		return "login";
 	}
-	
 }
